abae5721a70b1a5caa75ecaea7603867b300fac3
Yaman Almahamid <yamanmid@hotmail.com>
2014-05-26 11:12:18 -0700

    adding mongo db init and schemas

---
+++ server/init.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/shiplogic');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // todo
+});
+
+var address = mongoose.Schema({
+	street1: String,
+	street2: String,
+	street3: String,
+	city: String,
+	county: String,
+	state: String,
+	postalCode: String
+});
+
+var item = mongoose.Schema({
+	itemDescription: String,
+	SKUnumner: String,
+	Weight: Number,
+	Dimensions: Number
+});
+
+var fourmeSchema = mongoose.Schema({
+    firstName: String,
+    middleInitial: String,
+    lastName: String,
+    shipperAddress: [address],
+    recipientAddress: [address],
+    items : [[item]],
+    orderSummary,
+    numberOfItems: Number,
+    OrderValue: Number,
+    ServiceType: String
+});
